613b0608255281e7ca9d3a53e4519c7a02ed44b4
P <madivalp67@gmail.com>
2026-03-27 15:25:39 +0530

    Reverse string

---
+++ Reverse.java
@@ -0,0 +1,17 @@
+//Reverse a Number
+import java.util.*;
+public class Reverse {
+    public static void main(String []args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter Number To Reverse :");
+      int num =sc.nextInt();
+      int rev = 0;
+        while(num > 0){
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num = num / 10;
+
+        }System.out.println("Reversed Number: " + rev);
+    }
+}
