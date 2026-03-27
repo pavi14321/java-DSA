6754f683529046f077d678078149adb682b311e8
P <madivalp67@gmail.com>
2026-03-27 15:56:21 +0530

    Sum

---
+++ SumofDigits.java
@@ -0,0 +1,22 @@
+//Sum of Digits
+import java.util.Scanner;
+
+public class SumofDigits {
+ public static void main(String args[]) {
+    Scanner sc =  new Scanner(System.in);
+    System.out.println("Enter Number To add/Sum: ");
+    int num = sc.nextInt();
+    int sum = 0;
+    while( num > 0) {
+        int digit = num % 10;
+        sum = sum  + digit;
+        num = num  / 10;
+
+    }
+
+System.out.println("Sum of Digits : "+ sum);
+sc.close(); 
+ }  
+ 
+}
+
