0eea794af6b5b3d6c07f4b1c84fb6cc886c9e86b
P <madivalp67@gmail.com>
2026-03-27 13:03:41 +0530

    Odd even

---
+++ OddorEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class OddorEven {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number To Chech Odd or Even: ");
+        int n = sc.nextInt();
+          if(n % 2 == 0) {
+    System.out.println("The Number is Even");
+}
+else {
+    System.out.println("The Number is Odd");
+}
+}
+    }
+
