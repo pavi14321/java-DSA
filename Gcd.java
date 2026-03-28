ce80e90e3af33c4c7ba98c2079fdb03366ff21e3
P <madivalp67@gmail.com>
2026-03-28 11:41:18 +0530

    Gcd

---
+++ Gcd.java
@@ -0,0 +1,20 @@
+/*Your task is to find the Greatest Common Divisor (GCD) (also known as Highest Common Factor - HCF) of these two numbers. */
+
+import java.util.Scanner;
+
+public class Gcd {
+ public static int gcd(int a, int b) {
+    while(b != 0) {
+        int temp = b;
+        b = a %b;
+        a= temp;
+    }
+    return a;
+ }
+ public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(gcd(a,b));
+ }
+}
