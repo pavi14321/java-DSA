ec1bbecb92f9a2373a484561327f3153ebe8547d
P <madivalp67@gmail.com>
2026-04-03 17:04:52 +0530

    www

--- Patren6.java
+++ Patren6.java
@@ -7,6 +7,13 @@ public class Patren6 {
              }
              System.out.println();
             }
+            System.out.println("-------------------");
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n-i+1; j++) {
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
         }
     }
     
