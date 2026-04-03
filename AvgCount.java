46f9190f9236db125ef0a54b7c2e28e0c41b755d
Praveen <madivalp67@gmail.com>
2026-04-03 20:58:50 +0530

    java

---
+++ AvgCount.java
@@ -0,0 +1,28 @@
+/*Count Elements Greater Than Previous Average
+Given an array of positive integers, return the number of elements that are strictly greater than the average of all previous elements. Skip the first element.
+
+Example
+
+Input
+
+responseTimes = [100, 200, 150,300]
+Output
+
+2 */
+
+import java.util.Scanner;
+public class AvgCount {
+    public static int countavg(int[] arr) {
+        if(arr.length <=1) {
+            return 0;
+        
+        }
+        int count = 0;
+        int sum = arr[0];
+        for(int i = 0; i< arr.length; i++) {
+            
+        }
+
+    }
+    
+}
