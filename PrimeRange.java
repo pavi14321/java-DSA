9eb4210a2c3e024a1e06c14be5c5171c7df421bd
Praveen <madivalp67@gmail.com>
2026-04-14 20:35:19 +0530

    mnsdbc

---
+++ PrimeRange.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeRange {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start number: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end number: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+
+        for (int i = start; i <= end; i++) {
+            if (i <= 1) continue; // skip 0 and 1
+
+            boolean isPrime = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
