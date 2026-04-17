8a3a6ffd5e384c852fc85b3a4294edb21148de8a
Praveen <madivalp67@gmail.com>
2026-04-17 21:55:52 +0530

    hjshs

---
+++ FindArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class FindArray {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Size Of Array: ");
+        int size = sc.nextInt();
+        int []arr = new int[size];
+
+        System.out.println("Enter The Elements Of Array: ");
+        for(int i = 0; i<size; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Enter The Eliment To Search: ");
+        int find = sc.nextInt();
+
+        for(int i = 0; i<size; i++) {
+            if(arr[i] == find) {
+                System.out.println("Element Found At Index: " + i);
+                return;
+            }
+        }
+        System.out.println("Element Not Found");
+    }
+}
