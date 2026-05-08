3a779c0aadbdc7dc6c482ff17d07b6f9d7b36c32
Praveen <madivalp67@gmail.com>
2026-05-08 22:04:16 +0530

    sascasas

--- javaString.java
+++ javaString.java
@@ -1,13 +1,23 @@
 import java.util.*;
 public class javaString {
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your First name: ");
-        String fname = sc.nextLine();
-        System.out.print("Enter your Second name: ");
-        String sname = sc.nextLine();
-        String fullname = fname + " "  +sname;
-        System.out.print("your Full name is : "+fullname);
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter Your First name: ");
+        // String fname = sc.nextLine();
+        // System.out.print("Enter your Second name: ");
+        // String sname = sc.nextLine();
+        // String fullname = fname + " "  +sname;
+        // System.out.print("your Full name is : "+fullname);
+        // System.out.println();
+        // System.out.println(fullname.length());
+        String str1 = "hello";
+        String str2 = "hello";
+        if(str1.compareTo(str2) == 0) {
+            System.out.println("String are equal");
+        }
+        else {
+            System.out.println("String are not equal");
+        }
 
     }
     
