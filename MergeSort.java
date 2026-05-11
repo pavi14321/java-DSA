f892a0808cc2762625d2fe48a59f26ccdfc7b4a5
P <madivalp67@gmail.com>
2026-05-11 13:07:03 +0530

    jkh

---
+++ MergeSort.java
@@ -0,0 +1,11 @@
+public class MergeSort {
+
+    public static void divide(int arr[], int si, int ei) {
+        
+    }
+    public static void main(String args[]) {
+        int arr[] = {38, 27, 43, 3, 9, 82};
+        int n = arr.length;
+        
+    }
+}
